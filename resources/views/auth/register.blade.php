f181626dbd6600056d52199c7fc88f789ca74e8b
jessica <jessicacristina.ccg@gmail.com>
2017-08-06 00:57:10 -0300

    laravel scout ((:

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,32 +7,33 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Cadastro</div>
                 <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
+                    <form class="form-horizontal" id="register" role="form" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">Nome</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="name" type="text" class="form-control" onkeypress="return txtBoxFormat(event);" minlength="3" name="name" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
+                                        <strong><?php echo('Insira um nome');?></strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
+
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control required" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong><?php echo "Este endereço de email não é válido"; ?></strong>
                                     </span>
                                 @endif
                             </div>
@@ -46,7 +47,7 @@
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
+                                        <strong><?php echo "A senha deve ter 6 digitos"; ?></strong>
                                     </span>
                                 @endif
                             </div>
@@ -66,6 +67,14 @@
                             <div class="col-md-6">
                                 <input id="tipo" type="radio" name="tipo" value="0">Clínica
                                 <input id="tipo" type="radio" name="tipo" value="1">Usuário
+
+                            @if ($errors->has('tipo'))
+                                    <span class="help-block">
+                                        <strong><?php echo "Selecione um tipo"; ?></strong>
+                                    </span>
+                                @endif
+
+
                             </div>
                         </div>
 
@@ -82,4 +91,28 @@
         </div>
     </div>
 </div>
+<script>
+    
+    function txtBoxFormat(evtKeyPress) {
+        var i, nCount, sValue, fldLen, mskLen,bolMask, sCod, nTecla;
+
+        if(document.all) { // Internet Explorer
+            nTecla = evtKeyPress.keyCode;
+        } else if(document.layers) { // Nestcape
+            nTecla = evtKeyPress.which;
+        } else {
+            nTecla = evtKeyPress.which;
+            if (nTecla == 8) {
+                return true;
+            }
+        }
+        if (nTecla != 8)
+          return ((nTecla <= 47) || (nTecla >= 58)); 
+        else 
+          return true;
+    }
+
+
+</script>
+
 @endsection
